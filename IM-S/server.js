8d4d52381c88125f4b38823f43ce3cf864d33931
zuoqing <328103224@qq.com>
2018-04-10 23:28:34 +0800

    set server

--- IM-S/server.js
+++ IM-S/server.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-server.listen(13001);
+server.listen(13000);
 
 app.get('/', function (req, res) {
     res.sendfile(__dirname + '/www/index.html');
@@ -11,26 +11,25 @@ app.get('/', function (req, res) {
 var userList=[];
 
 io.on('connection', function (socket) {
-    console.log(socket.id+'登陆了！')
+    socket.socketID = socket.id;
     socket.on('msg',function(e){
-        console.log(e)
         if(e.toUserId){
             io.to(e.toUserId).emit('private msg', e)
         }else{
-
             io.sockets.emit('msg',e)
         }
     })
 
     socket.on('new user',function(e){
+
         e.userId=socket.id
         userList.push(e)
         io.sockets.emit('member list', userList);
         io.sockets.emit('login', e);//给指定的客户端发送消息
     })
 
-    socket.on('disconnect', function(){
-        console.log('connection is disconnect!');
+    socket.on('disconnect', function(e){
+        console.log(socket.socketID+'----connection is disconnect!');
     });
 
 });
